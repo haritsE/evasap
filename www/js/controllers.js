c1abfa906d2b3de55b83791aa798cacf287c370d
Sonny Lazuardi <sonnylazuardi@gmail.com>
2015-10-24 21:25:32 +0700

    update :

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -56,134 +56,8 @@ angular.module('versinfocus.controllers', ['ionic'])
   // },window);
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
-
 .controller('HomeCtrl', function($scope, ArchiveImage, Commodities, $ionicPopover) {
-  $scope.commodities = Commodities;
-  $scope.commodities.$loaded(function (snaphot) {
-    console.log($scope.commodities);
-  });
-  $ionicPopover.fromTemplateUrl('templates/homepopover.html', {
-      scope: $scope,
-  }).then(function(popover) {
-      $scope.popover = popover;
-  });
 
-  $scope.menu = {
-    searchActive: false,
-    toggleSearch: function() {
-      $scope.menu.searchActive = !$scope.menu.searchActive;
-    }
-  }
-
-})
-
-.service('VerseService', function(Verse, Favorites, $sce, $cordovaSocialSharing) {
-  return function($scope) {
-    $scope.isLike = false;
-    $scope.verse = {};
-
-    $scope.like = function() {
-      console.log('like');
-      $scope.isLike = !$scope.isLike;
-      if ($scope.isLike) {
-        Favorites.add($scope.verse);
-      } else {
-        Favorites.remove($scope.verse);
-      }
-    }
-
-    $scope.share = function() {
-      $cordovaSocialSharing
-        .shareViaFacebook("Read encouraging words here", null, "http://warungsatekamu.org/")
-        .then(function(result) {
-          alert('Sharing succeed.');
-        }, function(err) {
-          alert('Sharing failed.');
-        });
-    }
-  };
-})
-
-.controller('VerseCtrl', function($scope, $stateParams, Verse, Favorites, $sce, VerseService) {
-  VerseService($scope);
-
-  Verse.findById($stateParams.id).then(function (data) {
-    $scope.verse = data;
-    $scope.verse.content = $sce.trustAsHtml($scope.verse.content);
-    $scope.isLike = Favorites.isLike($scope.verse);
-  });
-})
-
-.controller('TodayCtrl', function($scope, Verse, Favorites, VerseService) {
-  VerseService($scope);
-
-  Verse.findByToday().then(function (data) {
-    $scope.verse = data;
-    $scope.isLike = Favorites.isLike($scope.verse);
-  });
-
-})
-
-.controller('DevotionsCtrl', function($scope, Devotions, $stateParams, $ionicLoading, $timeout) {
-  $scope.devotions = [];
-  $scope.page = 1;
-  $scope.finish = false;
-
-  $scope.loadMore = function() {
-    if (!$scope.finish) {
-      $ionicLoading.show({
-        template: 'Loading...'
-      });
-      Devotions.findByMonth($stateParams.month, $stateParams.year, $scope.page).then(function(data) {
-        if (data.length == 0) {
-          $scope.finish = true;
-        }
-        $scope.devotions = Devotions.all($stateParams.month, $stateParams.year);
-        $scope.page++;
-        $scope.$broadcast('scroll.infiniteScrollComplete');
-        $timeout(function() {
-          $ionicLoading.hide();
-        }, 1500);
-      }, function () {
-        $ionicLoading.hide();
-      });
-    } else {
-      $ionicLoading.hide();
-    }
-  }
-  $scope.$on('$stateChangeSuccess', function() {
-    $scope.loadMore();
-  });
-})
-
-.controller('FavoritesCtrl', function($scope, Favorites, $stateParams) {
-  $scope.devotions = [];
-  $scope.load = function() {
-    Favorites.getAll().then(function(data) {
-      // console.log('happy');
-      console.log(data);
-      $scope.devotions = data;
-    });
-  }
-  $scope.$on('$ionicView.beforeEnter', $scope.load);
-  $scope.confirmDelete = function(item) {
-    console.log(item);
-    Favorites.remove(item);
-    $scope.load();
-  }
-})
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
 })
 
 .controller('LoginCtrl', function($scope, $state, $firebaseAuth, $cordovaOauth, FBURL, Auth, AuthHelper) {
@@ -239,284 +113,116 @@ angular.module('versinfocus.controllers', ['ionic'])
   }
 })
 
+.service('MapInit', function ($cordovaGeolocation, $ionicSideMenuDelegate) {
+  var self = this;
+  self.init = function($scope) {
+    $ionicSideMenuDelegate.canDragContent(false);
+    var lat  = '-6.2398054';
+    var long = '106.8113921';
+    $scope.map = {center: {latitude: lat, longitude: long }, zoom: 16 };
+    $scope.options = {
+      scrollwheel: false,
+      overviewMapControl: false,
+      panControl: true,
+      scaleControl: true,
+      scrollwheel: false,
+      mapTypeControl: false,
+      streetViewControl: false,
+      zoomControl: true
+    };
+  }
+})
+
 .controller('MarketCtrl', function($scope, $state, $stateParams, $ionicSideMenuDelegate) {
-  $ionicSideMenuDelegate.canDragContent(false);
-  var lat  = '-6.2398054';
-  var long = '106.8113921';
-  $scope.map = {center: {latitude: lat, longitude: long }, zoom: 16 };
-  $scope.options = {
-    scrollwheel: false,
-    overviewMapControl: false,
-    panControl: true,
-    scaleControl: true,
-    scrollwheel: false,
-    mapTypeControl: false,
-    streetViewControl: false,
-    zoomControl: true
-  };
-  $scope.markets = [{
+  
+  $scope.markets = [];
+
+})
+
+.controller('LaporCtrl', function ($scope, $http, FBURL, $ionicSideMenuDelegate, MapInit, $cordovaCamera, $cordovaGeolocation) {
+  $scope.data = {};
+  MapInit.init($scope);
+
+  $scope.marker = {
     id: 1,
     coords: {
       latitude: -6.2398054,
       longitude: 106.8113921
     },
-    options: { draggable: false },
-  },{
-    id: 2,
-    coords: {
-      latitude: -6.2398054,
-      longitude: 106.8114000
-    },
-    options: { draggable: false },
-  },{
-    id: 3,
-    coords: {
-      latitude: -6.2430470,
-      longitude: 106.8247076
-    },
-    options: { draggable: false },
-  }];
-
-  $scope.test = {
-    forceToMarket : function(){
-    console.log("HEHE");
-    $state.go('app.marketSingle');
-  }}
-})
-
-.controller('MarketSingleCtrl', function($scope, $stateParams) {
-})
-
-.controller('NonMarketCtrl', function($scope, $state, $stateParams, $ionicSideMenuDelegate) {
-  $ionicSideMenuDelegate.canDragContent(false);
-  var lat  = '-6.2398054';
-  var long = '106.8113921';
-  $scope.map = {center: {latitude: lat, longitude: long }, zoom: 16 };
-  $scope.options = {
-    scrollwheel: false,
-    overviewMapControl: false,
-    panControl: true,
-    scaleControl: true,
-    scrollwheel: false,
-    mapTypeControl: false,
-    streetViewControl: false,
-    zoomControl: true
-  };
-  $scope.markets = [{
-    id: 1,
-    coords: {
-      latitude: -6.2398054,
-      longitude: 106.8113921
-    },
-    options: { draggable: false },
-  },{
-    id: 2,
-    coords: {
-      latitude: -6.2398054,
-      longitude: 106.8114000
-    },
-    options: { draggable: false },
-  },{
-    id: 3,
-    coords: {
-      latitude: -6.2430470,
-      longitude: 106.8247076
-    },
-    options: { draggable: false },
-  }];
-
-  $scope.test = {
-    forceToMarket : function(){
-    $state.go('app.nonMarketSingle');
-  }}
-})
-
-.controller('NonMarketSingleCtrl', function($scope, $stateParams) {
-})
-
-.controller('KirimKatalogCtrl', function($scope, Camera, $stateParams, $cordovaSms) {
-  $scope.data = {
-    title: "",
-    price: "",
-    stock: ""
-  };
-
-  $scope.test = {
-    submit : function(){
-      $state.go('app.home');
-    },
-    takeCamera : function(){
-      Camera.getPicture().then(function(imageURI) {
-        console.log(imageURI);
-        $scope.data.picture = imageURI;
-      }, function(err) {
-        console.err(err);
-      }, {
-        quality: 75,
-        targetWidth: 320,
-        targetHeight: 320,
-        saveToPhotoAlbum: false,
-        sourceType: 0,
-        correctOrientation: false
-      });
-    },
-    sms : function() {
-      var options = {
-          replaceLineBreaks: false, // true to replace \n by a new line, false by default
-          android: {
-              intent: 'INTENT'  // send SMS with the native android SMS messaging
-          }
-      };
-      $cordovaSms
-      .send('085722201351', $scope.data.title + " " + $scope.data.price + " " + $scope.data.stock, options)
-      .then(function() {
-        // Success! SMS was sent
-        console.log("Sukses");
-      }, function(error) {
-        // An error occurred
-        console.log("Error " + error);
-      });
-    }
-  }
-})
-
-.controller('LaporHargaCtrl', function($scope, Camera, $stateParams, $cordovaSms) {
-  $scope.data = {
-    title: "",
-    price: "",
-    market: ""
-  };
-
-  $scope.test = {
-    submit : function(){
-      $state.go('app.home');
-    },
-    takeCamera : function(){
-      Camera.getPicture().then(function(imageURI) {
-        console.log(imageURI);
-        $scope.data.picture = imageURI;
-      }, function(err) {
-        console.err(err);
-      }, {
-        quality: 75,
-        targetWidth: 320,
-        targetHeight: 320,
-        saveToPhotoAlbum: false,
-        sourceType: 0,
-        correctOrientation: false
-      });
-    },
-    sms : function() {
-      console.log("HI");
-      var options = {
-          replaceLineBreaks: false, // true to replace \n by a new line, false by default
-          android: {
-              intent: 'INTENT'  // send SMS with the native android SMS messaging
-              //intent: '' // send SMS without open any other app
-          }
-      };
-      $cordovaSms
-      .send('085722201351', $scope.data.title + " " + $scope.data.price + " " + $scope.data.market, options)
-      .then(function() {
-        // Success! SMS was sent
-        console.log("Sukses");
-      }, function(error) {
-        // An error occurred
-        console.log("Error " + error);
-      });
-    }
-  }
-})
-
-.controller('LaporPasarCtrl', function($scope, Camera, $stateParams, $ionicSideMenuDelegate, $cordovaSms) {
-  $ionicSideMenuDelegate.canDragContent(false);
-  $scope.data = {
-    title: "",
-    longitude: "",
-    latitude: ""
-  };
-
-  var lat  = '-6.2398054';
-  var long = '106.8113921';
-  $scope.map = {center: {latitude: lat, longitude: long }, zoom: 16};
-  $scope.options = {
-    scrollwheel: false,
-    overviewMapControl: false,
-    panControl: true,
-    scaleControl: true,
-    scrollwheel: false,
-    mapTypeControl: false,
-    streetViewControl: false,
-    zoomControl: true
-  };
-
-  $scope.test = {
-    submit : function(){
-      $state.go('app.home');
-    },
-    sms : function() {
-      console.log("HAI");
-      var options = {
-          replaceLineBreaks: false, // true to replace \n by a new line, false by default
-          android: {
-              intent: 'INTENT'  // send SMS with the native android SMS messaging
-              //intent: '' // send SMS without open any other app
-          }
-      };
-      $cordovaSms
-      .send('085722201351', $scope.data.title + " " + $scope.data.price + " " + $scope.data.market, options)
-      .then(function() {
-        // Success! SMS was sent
-        console.log("Sukses");
-      }, function(error) {
-        // An error occurred
-        console.log("Error " + error);
-      });
-    }
-  }
-
-})
-
-.controller('KontribusiCtrl', function($scope, $stateParams) {
-})
-
-.controller('PencapaianCtrl', function($scope, $stateParams) {
-})
-
-.controller('BantuanCtrl', function($scope, $stateParams) {
-})
-
-.controller('ShareCtrl', function($scope, $stateParams) {
-
-})
-
-.controller('TimelineCtrl', function($scope) {})
-
-.controller('CommoditySingleCtrl', function($scope) {
-  $scope.chartConfig = {
-    options: {
-      chart: {
-        type: 'line'
+    options: { draggable: true },
+    events: {
+      dragend: function (marker, eventName, args) {
+        var lat = marker.getPosition().lat();
+        var lon = marker.getPosition().lng();
+        $scope.data.latitude = lat;
+        $scope.data.longitude = lon;
+        // $scope.$apply();
       }
-    },
-    series: [{
-        data: [10, 15, 12, 8, 7]
-    }],
-    title: {
-        text: ''
-    },
-    loading: false,
-    yAxis: {
-      title: {
-        text: ''
-      },
-
-    },
-    xAxis: {
-
-    },
-    legend: {
-      enabled: false,
-    },
+    }
   };
-});
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var coords = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      }
+      console.log(coords);
+      $scope.marker.coords = coords;
+      $scope.map.center = coords;
+      $scope.data.latitude = coords.latitude;
+      $scope.data.longitude = coords.longitude;
+    });
+  } 
+  if ($cordovaGeolocation) {
+    var posOptions = {timeout: 10000, enableHighAccuracy: false};
+    $cordovaGeolocation
+      .getCurrentPosition(posOptions)
+      .then(function (position) {
+        console.log(position);
+        alert('masuk 2');
+        var coords = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        }
+        console.log(coords);
+        $scope.marker.coords = coords;
+        $scope.map.center = coords;
+        $scope.data.latitude = coords.latitude;
+        $scope.data.longitude = coords.longitude;
+      }, function(err) {
+        // error
+      });
+  }
+
+  $scope.submit = function () {
+    $http.post(FBURL + '/victims/.json', $scope.data)
+      .success(function() {
+        alert('success');
+      });
+  }
+
+  $scope.takePhoto = function() {
+    var options = {
+      quality: 80,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 500,
+      targetHeight: 360,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: true,
+      correctOrientation:true
+    };
+
+    $cordovaCamera.getPicture(options).then(function (imageData) {
+      alert('beres');
+      $scope.picture = "data:image/jpeg;base64," + imageData;
+    }, function(err) {
+      alert('error');
+    });
+  }
+})
+
+;
